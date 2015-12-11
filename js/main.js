d54adfc1a00269d1018545c1333e7fe4d972a188
Anjie Scheuren <emailanjie@gmail.com>
2015-12-11 12:16:29 -0600

    updated functions?

--- js/main.js
+++ js/main.js
@@ -3,83 +3,87 @@ $(function () {
 
   //make a variable = 0;
   var total = 0;
-  var empty;
 
-  //user input function
+  $('#clear').on('click', function() {
+    $('#total').text(0);
+  });
+
   $('#total').text(0);
 
   //addition function
+    //get user input
+    //put input in a variable
   $('#add').on('click', function() {
     var value1 = parseInt($('#userInput').val());
       total = total + value1;
 
-    //$('#total').text(0);
-
-    //get user input
-    //put input in a variable
+    //on click of equal button
+      //perform math variable
+      //return output
     $('#equal').on('click', function() {
       var value2 = parseInt($('#userInput').val());
       var newTotal = total + value2;
 
-    $('.addition').text(newTotal);
+    $('#total').text(newTotal);
     });
   });
 
   //subtraction function
+    //get user input
+    //put input in a variable
   $('#subtract').on('click', function() {
     var value1 = parseInt($('#userInput').val());
       total = total + value1;
 
-    //$('#total').text(0);
-
-    //get user input
-    //put input in a variable
+    //on click of equal button
+      //perform math variable
+      //return output
     $('#equal').on('click', function() {
       var value2 = parseInt($('#userInput').val());
       var newTotal = total - value2;
 
-    $('.subtraction').text(newTotal);
+    $('#total').text(newTotal);
 
     });
   });
 
   //multiplication funtion
+    //get user input
+    //put input in a variable
   $('#multiply').on('click', function() {
     var value1 = parseInt($('#userInput').val());
       total = total + value1;
 
-    //$('#total').text(0);
-
-    //get user input
-    //put input in a variable
+    //on click of equal button
+      //perform math variable
+      //return output
     $('#equal').on('click', function() {
       var value2 = parseInt($('#userInput').val());
       var newTotal = total * value2;
 
-    $('.multiplication').text(newTotal);
+    $('#total').text(newTotal);
     });
   });
 
   //division function
+    //get user input
+    //put input in a variable
   $('#divide').on('click', function() {
     var value1 = parseInt($('#userInput').val());
       total = total + value1;
 
-    //$('#total').text(0);
-
-    //get user input
-    //put input in a variable
+   //on click of equal button
+      //perform math variable
+      //return output
     $('#equal').on('click', function() {
       var value2 = parseInt($('#userInput').val());
       var newTotal = total / value2;
 
-    $('.division').text(newTotal);
+    $('#total').text(newTotal);
 
     });
   });
 
-    //on click of equal button
-      //perform math variable
-      //return output
+
 
 });
